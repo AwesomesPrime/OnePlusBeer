2b9ce778185ddefd94a8e6f82ba0b8aab4556ca8
Stefan <123@123.de>
2018-06-26 10:41:49 +0200

    Packages for ORM

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,6 +3,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import orm.GenericDatabaseService;
 
 import java.util.ArrayList;
 
