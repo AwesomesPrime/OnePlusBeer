3a462d0df107cce8814f820cf2a1f2a1f712b688
kamps <joel.kamps@peakwork.com>
2018-06-29 10:09:10 +0200

    correct MainMenu call

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -18,7 +18,7 @@ public class Main extends Application{
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("frontend/mainMenu/mainMenu.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("frontend/mainMenu/MainMenu.fxml"));
         primaryStage.setTitle("OnePlusBeer");
         primaryStage.setScene(new Scene(root, 1000, 800));
         primaryStage.show();
