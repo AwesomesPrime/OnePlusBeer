9c99d6fd7cefea3eda7240d90805e33292b3f162
Stefan <123@123.de>
2018-06-29 09:07:28 +0200

    refactoring for dbService

--- src/main/java/orm/EventDatabaseService.java
+++ src/main/java/orm/EventDatabaseService.java
@@ -14,24 +14,5 @@ import java.util.Date;
 public class EventDatabaseService extends GenericDatabaseService<Event> {
 
     public EventDatabaseService(){
-        ArrayList<Event> events = this.getAll(Event.class);
-        if(events.size() == 0){
-            Calendar startDate = Calendar.getInstance();
-            startDate.set(2018,11,1, 0, 0, 0);
-
-            Calendar endDate = Calendar.getInstance();
-            endDate.set(2018,11,23, 0, 0 ,0);
-
-            Event event = new Event( "Weihnachtsmarkt 2018", startDate.getTime() , endDate.getTime(), "Straße", 12, "1256", "Düsseldorf");
-            this.save(event);
-
-            startDate.set(2017,11,1, 0, 0, 0);
-
-            endDate.set(2017,11,23, 0, 0 ,0);
-
-            event = new Event( "Weihnachtsmarkt 2017", startDate.getTime() , endDate.getTime(), "Hauptstraße", 19, "40597", "MalleSüd");
-            System.out.println(endDate.getTime().toString());
-            this.save(event);
-        }
     }
 }
