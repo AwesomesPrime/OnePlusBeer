3d8aefda5ff4b1fdd445babfea0d4a2a15fd9fc3
Stefan <123@123.de>
2018-07-02 15:22:04 +0200

    to be done tommrow

--- src/main/java/orm/StandDatabaseService.java
+++ src/main/java/orm/StandDatabaseService.java
@@ -2,8 +2,18 @@ package orm;
 
 import entities.Stand;
 
+import java.util.ArrayList;
+import java.util.stream.Collectors;
+
 /**
  * Created by Ulokal on 29.06.2018.
  */
 public class StandDatabaseService extends GenericDatabaseService<Stand> {
+
+    public ArrayList<Stand> search (String term) {
+        ArrayList<Stand> allEvents = this.getAll(Stand.class);
+        ArrayList<Stand> resultEvents = allEvents.stream().filter(stand -> Integer.toString(stand.getId()).contains(term) ||
+                                                                  stand.getStreet().contains(term) ||
+                                                                  stand.getZip().contains(term)
+    }
 }
